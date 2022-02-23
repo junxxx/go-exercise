c1c131f70b982134c619ec0f3ceb2b9a2f69a5f0
junxxx <hprjunxxx@gmail.com>
2022-02-23 16:55:02 +0800

    rep status code

--- src/exercise-1.1/http/http.go
+++ src/exercise-1.1/http/http.go
@@ -5,9 +5,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func Get(url string) {
+	preFix := "http://"
+	if !strings.HasPrefix(url, preFix) {
+		url = preFix + url
+	}
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -15,8 +20,11 @@ func Get(url string) {
 	}
 
 	_, err = io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "copy failed: %v\n", err)
 		os.Exit(1)
 	}
+
+	fmt.Printf("\nresponse status: %d\n", resp.StatusCode)
 }
